666e53ac41008646b751f3c99271ccaa03b8761f
Lana555 <lana555@Lana555s-MBP.Dlink>
2017-12-13 22:05:34 +0400

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package lanadev.com.woweatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
